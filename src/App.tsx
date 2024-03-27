f4e4679b92100e8104af0debaee712c22ad7dad4
曲智垚 <1114994863@qq.com>
2024-03-27 21:54:38 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
+          <div>1</div>
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
